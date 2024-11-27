7dd2049d69ff973c0da14a0d23ab42d70aef0703
Aashish-Raj <aashishkumar7814@gmail.com>
2024-11-27 13:39:02 +0530

    add the  status  code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi import Depends
+from fastapi import status
 
 
 # create the FastApi Application
@@ -26,10 +27,11 @@ def home():
     return {"message":"Testingggg"}
 
 # create item
-@app.post('/items')
+@app.post('/items',response_model=Item,status_code=status.HTTP_201_CREATED)
 async def create_item(item:Item):
     # print('iitem:--->>>',query,limit)
-    return {'item_name':item.name,'price':item.price,'description':item.description,'tax':item.tax}
+    # return {'item_name':item.name,'price':item.price,'description':item.description,'tax':item.tax}
+    return item
 
 
 # add the  dependency in  fast api
